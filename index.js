729c6918047de9a1a7d234408d32f40cd2348b95
Francisco Mateus <32884516+ftmateus@users.noreply.github.com>
2021-08-03 15:29:30 +0000

    Navbar fixed, overlay moves acording to navbar

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ var modelView;
 
 let time_increments = [0, 1, 2, 3, 9]
 
-var global_time = 0;
+let execution_time = 0;
+var solar_system_time = 0;
 
 var start_time;
 
@@ -103,13 +104,15 @@ function fit_canvas_to_window()
 
     aspect = canvas.width / canvas.height;
     gl.viewport(0, 0,canvas.width, canvas.height);
-    if (global_time > 0)
-        if (time_increment == 0) animate();
 
 }
 
 window.onresize = function () {
     fit_canvas_to_window();
+    if(time_increment == 0) 
+    {   
+        animate();
+    }
 }
 
 function change_time_increment(inc)
@@ -165,11 +168,13 @@ window.onload = function() {
         planet_scale = document.getElementById("planetRange").value = 10;
         orbit_scale = document.getElementById("orbitRange").value = 0.025;
         orbit_scale_moons = 1*Math.exp(orbit_scale-1)*Math.exp((planet_scale-1)/100);
+        if(time_increment == 0) animate();
     });
     $('#realScale').bind('click', function() {
         planet_scale = document.getElementById("planetRange").value = 1;
         orbit_scale = document.getElementById("orbitRange").value = 1;
         orbit_scale_moons = 1*Math.exp(orbit_scale-1)*Math.exp((planet_scale-1)/100);
+        if(time_increment == 0) animate();
     });
     $('#help').bind('click', function() {
         alert(help_msg);
@@ -180,13 +185,14 @@ window.onload = function() {
         planet_scale = document.getElementById("planetRange").value;
         orbit_scale = document.getElementById("orbitRange").value;
         orbit_scale_moons = 1*Math.exp(orbit_scale-1)*Math.exp((planet_scale-1)/100);
+        if(time_increment == 0) animate();
     });
 
     // document.addEventListener("visibilitychange", function() {
     //     console.log(document.hidden, document.visibilityState);
     //   }, false);
 
-    render();
+    animate();
 }
 
 function addBodiesButtons()
@@ -194,7 +200,7 @@ function addBodiesButtons()
     
     for(const [_, body] of Object.entries(solar_system_data.bodies))
     {
-        createCelestialBodyButton(body);
+        createCelestialBodyButtonNav(body);
     }
 }
 
@@ -209,6 +215,24 @@ function createCelestialBodyButton(body)
     button.addEventListener("click", function() {center = body; if (time_increment == 0) animate();});
 }
 
+function createCelestialBodyButtonNav(body)
+{
+    const container = document.getElementById('planetsNavbarBtnsCont');
+    const li = document.createElement("li");
+    li.className += "nav-item"
+
+    const a = document.createElement("a");
+    a.className += "nav-link"
+    a.href="#"
+    a.innerText = body.name
+
+    li.appendChild(a);  
+    container.appendChild(li)
+
+    a.addEventListener("click", function() {center = body; if (time_increment == 0) animate();});
+}
+
+
 
 function loadSolarSystem()
 {
@@ -220,11 +244,10 @@ function loadSolarSystem()
         },
         async: false
     })
-    console.log(solar_system_data);
-    center = solar_system_data.bodies[solar_system_data.properties.center];
-    VP_DISTANCE = solar_system_data.properties.vp_distance
+    center = solar_system_data.bodies[solar_system_data.center];
+    VP_DISTANCE = solar_system_data.vp_distance
     //zoom = VP_DISTANCE/solar_system_data.bodies.Earth.orbit;
-    zoom = solar_system_data.properties.zoom
+    zoom = solar_system_data.zoom
 }
 
 function switchShading()
@@ -353,7 +376,7 @@ function moveCamera()
     var projection =mult(ortho(-VP_DISTANCE*aspect,VP_DISTANCE*aspect, -VP_DISTANCE, VP_DISTANCE,-100*VP_DISTANCE,100*VP_DISTANCE),
     scalem(zoom, zoom, 1));
     
-    var theta = center.year == 0 ? 0 : radians(global_time/center.year);
+    var theta = center.year == 0 ? 0 : radians(solar_system_time/center.year);
     
     var x = center.orbit*Math.cos(theta)*orbit_scale;
     var y = center.orbit*Math.sin(theta)*orbit_scale;
@@ -379,7 +402,9 @@ function animate()
             default: console.assert(false, "Unknown body type!")
         }
     }
-    global_time += time_increment;
+    solar_system_time += time_increment;
+
+    execution_time++;
 
     renderOverlay();
 
@@ -406,11 +431,11 @@ function addPlanet(planet)
 {
     gl.uniform1i(sunLoc, 0);
     pushMatrix();
-        multRotationY(global_time/planet.year);
+        multRotationY(solar_system_time/planet.year);
         multTranslation([planet.orbit*orbit_scale, 0, 0]);
         pushMatrix();
             multScale([ planet.diameter*planet_scale, planet.diameter*planet_scale, planet.diameter*planet_scale]);
-            multRotationY(global_time/planet.day);
+            multRotationY(solar_system_time/planet.day);
             drawPlanet(planet.color, textures && isFilled ? planet.texture : null, planet.distance);
         popMatrix();
         if (planet.rings)
@@ -427,7 +452,7 @@ function addPlanet(planet)
             {
                 pushMatrix();
                     multRotationX(-30);
-                    multRotationY(global_time/moon.year);
+                    multRotationY(solar_system_time/moon.year);
                     multTranslation([moon.orbit*orbit_scale_moons + planet.diameter/2*planet_scale*Math.log(planet_scale), 0, 0]);
                     // if (planet == planets.SATURN)
                     // {
@@ -449,7 +474,7 @@ function addStar(star)
     pushMatrix();
         pushMatrix();
         multScale([ star.diameter, star.diameter, star.diameter]);
-        multRotationY(global_time/star.day);
+        multRotationY(solar_system_time/star.day);
             drawPlanet(vec4(1.0, 1.0, 1.0, 1.0), textures && isFilled  ? star.texture : null, 0);
         popMatrix();
     popMatrix();
@@ -481,10 +506,10 @@ function drawRings()
 
 function renderOverlay()
 {
-    document.getElementById("days").textContent = parseInt((global_time/360));
-    document.getElementById("months").textContent = parseInt((global_time/360)/(EARTH_YEAR/12));
-    document.getElementById("years").textContent = parseInt((global_time/360)/EARTH_YEAR);
-    if (time_increment != 0)
-        document.getElementById("seconds_start").textContent = parseInt(((global_time/time_increment)/REFRESH_RATE));//(global_time/360)/EARTH_YEAR;
+    document.getElementById("days").textContent = parseInt((solar_system_time/360));
+    document.getElementById("months").textContent = parseInt((solar_system_time/360)/(EARTH_YEAR/12));
+    document.getElementById("years").textContent = parseInt((solar_system_time/360)/EARTH_YEAR);
+    
+    document.getElementById("seconds_start").textContent = parseInt((execution_time/REFRESH_RATE));//(global_time/360)/EARTH_YEAR;
     document.getElementById("shading_method").textContent = currentProgram == programPhong ? "Phong" : "Gouraud";
 }
