b5a522644a64fdfb22c346514b5c4f83a0964381
Francisco Mateus <32884516+ftmateus@users.noreply.github.com>
2021-08-07 14:43:40 +0000

    Render again when planet texture is completely mapped

--- index.js
+++ index.js
@@ -308,6 +308,7 @@ function setupTexture(imagesrc)
         gl.bindTexture(gl.TEXTURE_2D, texture);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image);
         gl.generateMipmap(gl.TEXTURE_2D);
+        animate();
     });
 
     return texture;
